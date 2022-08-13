4b57dd1f8a68ffe30d69345b2cc13ddea8938013
Y3A <62646606+Y3A@users.noreply.github.com>
2022-08-12 21:51:06 -0700

    Add files via upload

---
+++ shellcode_rdi/srdi.h
@@ -0,0 +1,21 @@
+#ifndef SRDI_H
+#define SRDI_H
+
+#define SRDI_CALLOFFSET 0x00000001
+
+#define kill(...) return printf(__VA_ARGS__), 0
+
+typedef struct
+{
+
+    DWORD   opt;
+    char    filepath[MAX_PATH + 1];
+    DWORD   offset;
+
+} BUILDER_ARGS, *PBUILDER_ARGS;
+
+
+int build_shellcode(PBUILDER_ARGS args);
+void _strncpy(char *dest, char *src, size_t len);
+
+#endif
